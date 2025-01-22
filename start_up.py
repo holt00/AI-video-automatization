3bc6fc725e59dddb566627dcdde347c30fc9c56d
CARLOS MARTÍNEZ JAÉN <Carlos.Martinez57@alu.uclm.es>
2025-01-22 23:58:42 +0100

    Added basic imports and enviroment variable readings

---
+++ start_up.py
@@ -0,0 +1,8 @@
+#Importing all the necessary libraries and modules
+from openai import OpenAI #OpenAI api client
+import os
+from dotenv import load_dotenv #module to load openai api key from .env file
+
+load_dotenv();  #geting the api key from .env file
+
+#
